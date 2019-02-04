2e14d514676e0963806dc6b908d6eb897d290b95
Marco Galicia <galprasmarco@gmail.com>
2019-02-04 13:12:07 -0600

    se borro el script bajar-solicitudes por sustitución de los otros dos scripts

--- bajar-solicitudes.sh
+++
@@ -1,3 +0,0 @@
-wget http://www.infomex.org.mx/gobiernofederal/HomeOpenDataServlet?opcion=Todos.CSV -O  todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
-unzip todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
-
