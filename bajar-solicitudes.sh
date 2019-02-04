0435c3f4159d2bbf010527b7a2f3540b3288bed9
Marco Galicia <galprasmarco@gmail.com>
2019-02-04 13:10:03 -0600

    se separaron los script de bajar y unificar

---
+++ bajar-solicitudes.sh
@@ -0,0 +1,3 @@
+wget http://www.infomex.org.mx/gobiernofederal/HomeOpenDataServlet?opcion=Todos.CSV -O  todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
+unzip todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
+
