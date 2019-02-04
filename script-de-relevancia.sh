bb32af103e4c660a6c4039f4ab4b5bdc20d9bcc5
Marco Galicia <galprasmarco@gmail.com>
2019-02-04 13:04:47 -0600

    simplifiqué el script de relevancia, gracias a que se trabajó en hacer queries más específicas en el script de búsqueda, solo se encarga de elininar solicitudes duplicadas y crear el archivo final

--- script-de-relevancia.sh
+++ script-de-relevancia.sh
@@ -1,12 +1,4 @@
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep -v '^\s*1'  >> solicitudes-relevantes.csv
-# la búsqueda de arriba omite los siguientes términos.
-## las siguientes líneas los agregan
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*10'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*11'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*12'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*13'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*14'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*15'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*16'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*17'  >> solicitudes-relevantes.csv
-cat solicitudes-ddhh-independiente.csv | sort | uniq --count | grep  '^\s*19'  >> solicitudes-relevantes.csv
+## deduplicación y conteo de las solicitudes. Da un cierto número por relevancia
+## agregar cabecera
+head -n 1 sol-2003-2018.csv > solicitudes-inai-ddhh.csv
+cat solicitudes-ddhh-independiente.csv | sort | uniq --count >> solicitudes-inai-ddhh.csv
