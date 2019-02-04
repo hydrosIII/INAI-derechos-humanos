c4b62bd9664d63f6a6db1c010da19da37dd81b01
Marco Galicia <galprasmarco@gmail.com>
2019-02-04 13:14:36 -0600

    de nuevo

---
+++ bajar-solicitudes.sh
@@ -0,0 +1,2 @@
+wget http://www.infomex.org.mx/gobiernofederal/HomeOpenDataServlet?opcion=Todos.CSV -O  todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
+unzip todas-las-solicitudes-de-acceso-a-ala-informacion-2003-2018.zip
