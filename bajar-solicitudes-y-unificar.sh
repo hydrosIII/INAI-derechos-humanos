dfa0e3e9f20344ab0e139a2a71350021c8bf7afa
hydrosIII <galprasmarco@gmail.com>
2019-02-04 13:06:33 -0600

    correción en comentario

--- bajar-solicitudes-y-unificar.sh
+++ bajar-solicitudes-y-unificar.sh
@@ -13,7 +13,7 @@ iconv -f ISO-8859-1 -t UTF8 solicitudes2005.csv -o solicitudes2005utf8.csv
 iconv -f ISO-8859-1 -t UTF8 solicitudes2006.csv -o solicitudes2006utf8.csv
 iconv -f ISO-8859-1 -t UTF8 solicitudes2007.csv -o solicitudes2007utf8.csv
 iconv -f ISO-8859-1 -t UTF8 solicitudes2008.csv -o solicitudes2008utf8.csv
-#### El archivo del 2009 bajado en enero del 2009 de infomex es inválido y 
+#### El archivo del 2009 bajado en enero del 2019 de infomex es inválido y 
 #### no sirve. Por lo que este repositorio provee un archivo anterior de 
 #### noviembre del 2018 que es válido y es el que se usa en la construcción 
 #### del archivo unificado. Si se quiere usar el original bajado, se puede borrar la 
